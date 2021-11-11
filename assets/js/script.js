3362cdcb683a884bbf22aa50da0c31f5248ea05c
Troy Allen <jake.allen@me.com>
2021-11-11 15:53:17 -0500

    responsive changes viable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const workSection = document.getElementById('work');
+const workSection = document.getElementsByClassName('work');
 const workCards = document.getElementsByClassName('workCard');
 
 function makeVisible(element){
@@ -6,23 +6,24 @@ function makeVisible(element){
         element.style.opacity = "1";
         element.closest('.workCard').dataset.active = "true";
 }
-
-workSection.addEventListener('click', function(e){
-    for (let x = 0; x < workCards.length; x++) {
-        if (workCards[x].dataset.active == "true") {
-            workCards[x].lastElementChild.style.opacity = "0";
-            workCards[x].lastElementChild.style.visibility = 'hidden'
-            workCards[x].dataset.active = "false";
+for (const section of workSection) {
+    section.addEventListener('click', function(e){
+        for (let x = 0; x < workCards.length; x++) {
+            if (workCards[x].dataset.active == "true") {
+                workCards[x].lastElementChild.style.opacity = "0";
+                workCards[x].lastElementChild.style.visibility = 'hidden'
+                workCards[x].dataset.active = "false";
+            }
         }
-    }
-    if (e.target.matches('.workCard')) {
-        const element = e.target.lastElementChild;
-        makeVisible(element);
-        return;
-    }
-    if (e.target.closest('.workCard')) {
-        const element = e.target.parentElement.nextElementSibling
-        makeVisible(element);
-        return;
-    }
-})
+        if (e.target.matches('.workCard')) {
+            const element = e.target.lastElementChild;
+            makeVisible(element);
+            return;
+        }
+        if (e.target.closest('.workCard')) {
+            const element = e.target.parentElement.nextElementSibling
+            makeVisible(element);
+            return;
+        }
+    })
+}
