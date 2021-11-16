7ba4de8652e3347538a3b07c730fada2aa62adc8
Troy Allen <jake.allen@me.com>
2021-11-16 09:43:31 -0500

    testing features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,26 @@
 const workSection = document.getElementsByClassName('work');
 const workCards = document.getElementsByClassName('workCard');
+const navLinks = document.querySelectorAll('.navLink')
+const articles = document.querySelectorAll('article')
 
 function makeVisible(element){
     element.style.visibility = "visible";
         element.style.opacity = "1";
         element.closest('.workCard').dataset.active = "true";
 }
+
+for (const link of navLinks){
+    link.addEventListener('click', e => {
+        articles.forEach(article => {
+            if (article.id == e.target.dataset.link) {
+                article.style.display = 'flex';
+            } else {
+            article.style.display = 'none';
+            }
+    })
+})
+}
+
 for (const section of workSection) {
     section.addEventListener('click', function(e){
         for (let x = 0; x < workCards.length; x++) {
