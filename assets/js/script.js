8102c7683d5c386d50d8290411793c0513413079
Troy Allen <jake.allen@me.com>
2021-11-16 10:57:27 -0500

    added fade in scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,25 @@ const workSection = document.getElementsByClassName('work');
 const workCards = document.getElementsByClassName('workCard');
 const navLinks = document.querySelectorAll('.navLink')
 const articles = document.querySelectorAll('article')
+const jumbotron = document.getElementById('jumbotron');
+
+function handleIntersection(entries) {
+    entries.map((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+        } else {
+            entry.target.style.opacity = 0;
+        }
+    })
+}
+const IO = new IntersectionObserver(handleIntersection);
+
+articles.forEach(article => IO.observe(article))
 
 function makeVisible(element){
     element.style.visibility = "visible";
         element.style.opacity = "1";
         element.closest('.workCard').dataset.active = "true";
-}
-
-for (const link of navLinks){
-    link.addEventListener('click', e => {
-        articles.forEach(article => {
-            if (article.id == e.target.dataset.link) {
-                article.style.display = 'flex';
-            } else {
-            article.style.display = 'none';
-            }
-    })
-})
 }
 
 for (const section of workSection) {
